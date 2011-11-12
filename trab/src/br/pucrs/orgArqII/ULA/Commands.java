683eef09245e6908d7b3f7182f17630d33007f95
Matheus Bernardes <matheusjardimb@gmail.com>
2011-11-12 15:42:32 -0200

    Added Utils.java

---
+++ trab/src/br/pucrs/orgArqII/ULA/Commands.java
@@ -0,0 +1,8 @@
+package br.pucrs.orgArqII.ULA;
+
+public abstract class Commands {
+	
+	public static void calculate(){
+		
+	}
+}
---
+++ trab/src/br/pucrs/orgArqII/Utils/Utils.java
@@ -0,0 +1,9 @@
+package br.pucrs.orgArqII.Utils;
+
+import java.util.List;
+
+public class Utils {
+	public static List<String> readFile(String filePath) {
+		return null;
+	}
+}
