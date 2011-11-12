a30c6ead861be7b1260286582c1b77d29094c53d
Mauricio Dias <mau.dias@gmail.com>
2011-11-12 16:20:42 -0200

    dumb Utils

--- trab/src/br/pucrs/orgArqII/Utils/Utils.java
+++ trab/src/br/pucrs/orgArqII/Utils/Utils.java
@@ -1,9 +1,31 @@
 package br.pucrs.orgArqII.Utils;
 
 import java.util.List;
+import java.io.*;
 
 public class Utils {
+	/*
 	public static List<String> readFile(String filePath) {
 		return null;
+	}
+	*/
+	public List<String> readFile(String filePath) {
+		StringBuilder contents = new StringBuilder();
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(filePath));
+			try {
+				String line = null;
+				while ((line = in.readLine()) != null) {
+					contents.append(line);
+					contents.append(System.getProperty("line.separator"));
+				}
+			} finally {
+				in.close();
+			}
+		} catch (IOException ex) {
+		}
+
+		return contents.toString();
 	}
 }
