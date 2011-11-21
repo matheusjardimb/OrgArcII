eebb8fc89b2b20239ef0330dc7618eff416d71df
Mauricio Dias <mau.dias@gmail.com>
2011-11-21 19:13:54 -0200

    Instrucoes adicionadas: - AND, ANDI, OR, ORI, SLT, SLTI

--- trab/src/br/pucrs/orgArqII/MIPS/Command.java
+++ trab/src/br/pucrs/orgArqII/MIPS/Command.java
@@ -17,10 +17,16 @@ public class Command {
 				throw exception;
 			break;
 		case AND:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case ADDI:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case ANDI:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case BEQ:
 			break;
@@ -31,12 +37,20 @@ public class Command {
 		case NOP:
 			break;
 		case OR:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case ORI:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case SLT:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case SLTI:
+			if (params.size() != 3)
+				throw exception;
 			break;
 		case SW:
 			break;
@@ -65,15 +79,23 @@ public class Command {
 			Integer add3 = new Integer(registers.get(Registers.valueOf(this.params.get(2))));
 			registers.put(add1, add2 + add3 + "");
 			break;
-		case AND:
+		case AND: // and reg, reg, reg
+			Registers and1 = Registers.valueOf(this.params.get(0));
+			Integer and2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer and3 = new Integer(registers.get(Registers.valueOf(this.params.get(2))));
+			registers.put(and1, (and2 & and3) + "");
 			break;
-		case ADDI:// addi reg, reg, const
+		case ADDI: // addi reg, reg, const
 			Registers addi1 = Registers.valueOf(this.params.get(0));
 			Integer addi2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
 			Integer addi3 = new Integer(this.params.get(2));
 			registers.put(addi1, addi2 + addi3 + "");
 			break;
-		case ANDI:
+		case ANDI: // andi reg, reg, const
+			Registers andi1 = Registers.valueOf(this.params.get(0));
+			Integer andi2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer andi3 = new Integer(this.params.get(2));
+			registers.put(andi1, (andi2 & andi3) + "");
 			break;
 		case BEQ:
 			break;
@@ -83,13 +105,29 @@ public class Command {
 			break;
 		case NOP:
 			break;
-		case OR:
+		case OR: // or reg, reg, reg
+			Registers or1 = Registers.valueOf(this.params.get(0));
+			Integer or2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer or3 = new Integer(registers.get(Registers.valueOf(this.params.get(2))));
+			registers.put(or1, (or2 | or3) + "");
 			break;
-		case ORI:
+		case ORI: // ori reg, reg, const
+			Registers ori1 = Registers.valueOf(this.params.get(0));
+			Integer ori2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer ori3 = new Integer(this.params.get(2));
+			registers.put(ori1, (ori2 | ori3) + "");
 			break;
-		case SLT:
+		case SLT: // slt reg, reg, reg
+			Registers slt1 = Registers.valueOf(this.params.get(0));
+			Integer slt2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer slt3 = new Integer(registers.get(Registers.valueOf(this.params.get(2))));
+			registers.put(slt1, (slt2 < slt3) ? "1" : "0");
 			break;
-		case SLTI:
+		case SLTI: // slti reg, reg, const
+			Registers slti1 = Registers.valueOf(this.params.get(0));
+			Integer slti2 = new Integer(registers.get(Registers.valueOf(this.params.get(1))));
+			Integer slti3 = new Integer(this.params.get(2));
+			registers.put(slti1, (slti2 < slti3) ? "1" : "0");
 			break;
 		case SW:
 			break;
